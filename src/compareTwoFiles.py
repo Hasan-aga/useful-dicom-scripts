2c1b245a12fca5fe95863898d82e65778a8f9baf
Hasan-aga <careerforma@gmail.com>
2024-05-29 16:49:34 +0300

    print color and truncate long output

--- src/compareTwoFiles.py
+++ src/compareTwoFiles.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
 
+import os
 import sys
 
 import pydicom
+from termcolor import colored
+
+
+def truncate_value(value, length=100):
+    """Truncate the value if it is longer than the specified length."""
+    value_str = str(value)
+    return value_str if len(value_str) <= length else value_str[:length] + "..."
 
 
 def compare_dicom_tags(file1_path, file2_path):
     print(f"Comparing DICOM tags between {file1_path} and {file2_path}...")
     # Load the DICOM files
+        # Check if files exist
+    if not os.path.exists(file1_path):
+        print(f"Error: File {file1_path} not found.")
+        sys.exit(1)
+    if not os.path.exists(file2_path):
+        print(f"Error: File {file2_path} not found.")
+        sys.exit(1)
     dicom1 = pydicom.dcmread(file1_path)
     dicom2 = pydicom.dcmread(file2_path)
 
@@ -23,23 +38,23 @@ def compare_dicom_tags(file1_path, file2_path):
     differing_tags = {tag for tag in common_tags if tags1[tag].value != tags2[tag].value}
 
     # Print the comparison results
-    print("Common Tags:")
-    for tag in common_tags:
-        print(f"{tag}: {tags1[tag].name}")
+    # print("Common Tags:")
+    # for tag in common_tags:
+    #     print(f"{tag}: {tags1[tag].name}")
 
-    print("\nUnique Tags in File 1:")
-    for tag in unique_tags_file1:
-        print(f"{tag}: {tags1[tag].name}")
+    # print("\nUnique Tags in File 1:")
+    # for tag in unique_tags_file1:
+    #     print(f"{tag}: {tags1[tag].name}")
 
-    print("\nUnique Tags in File 2:")
-    for tag in unique_tags_file2:
-        print(f"{tag}: {tags2[tag].name}")
+    # print("\nUnique Tags in File 2:")
+    # for tag in unique_tags_file2:
+    #     print(f"{tag}: {tags2[tag].name}")
 
     print("\nDiffering Tags:")
     for tag in differing_tags:
         print(f"{tag}:")
-        print(f"  File 1: {tags1[tag].value}")
-        print(f"  File 2: {tags2[tag].value}")
+        print(colored(f"  File 1: {truncate_value(tags1[tag].value)}", 'red'))
+        print(colored(f"  File 2: {truncate_value(tags2[tag].value)}", 'green'))
 
 if __name__ == "__main__":
     if len(sys.argv) !=3:
